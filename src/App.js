5acb6f459cb927db92a4bc9a734668445869ef1e
Santosh Marigowda <santosharakere@gmail.com>
2018-11-30 06:49:31 +0000

    just a button

--- src/App.js
+++ src/App.js
@@ -7,27 +7,15 @@ function App() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React - { counter }
-          </a>
-          <br/>
           <button 
             style={{ 
-              backgroundColor: 'red',
+              backgroundColor: 'blue',
               width: '150px',
               height: '50px',
               fontSize: '18px'
             }}
-            onClick={() => { useCounter(counter+1)} }>Increment</button>
+            onClick={() => { useCounter(counter+1)} }>Click Me: {counter}</button>
+          <br />
         </header>
         
       </div>
